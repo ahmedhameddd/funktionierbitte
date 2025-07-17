51380ffa23bc277166aad5b5b680887814ddb571
ahmedhameddd <0128ahmedhamed@gmail.com>
2025-07-17 21:35:02 +0200

    Delete app.js

--- app.js
+++
@@ -1,3 +0,0 @@
-function gruss() {
-  alert("I love you so much. Hoffe du vergisst nie wie wichtig du mir bist. Btw, bei unserem nächsten treffen werd ich dich 10 mal mehr umarmen. Freu mich so sehr auf unsere gemeinsame Zukunft");
-}
